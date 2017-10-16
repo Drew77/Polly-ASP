c156190476d6d237aee6be69832ff02e2070d7d9
Drew <mrdrewy_@hotmail.com>
2017-10-16 20:44:21 +0100

    Add project files.

---
+++ Polly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Polly.Startup))]
+namespace Polly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
